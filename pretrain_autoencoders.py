859f567b36f9beedaa2ba80eac133f2369abe2e9
Mehdi Benabed <mehdi.benabed@live.fr>
2023-10-24 18:55:37 +0200

    test: testing luke vc

--- pretrain_autoencoders.py
+++ pretrain_autoencoders.py
@@ -16,7 +16,7 @@ from model.stacked_ae import StackedAE
 from utils.train import validate_autoencoder
 from copy import deepcopy
 from utils.utils import random_seed
-#test luke connection
+#test luke connection 2
 
 
 def pretrain(flags):
